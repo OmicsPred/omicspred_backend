7ca7d37234554ea5d27d778f79990a0b9b6015ff
Laurent Gil <lg10@sanger.ac.uk>
2024-11-04 10:00:13 +0000

    Update indexes and Reactome imports

--- search_es/indexes.py
+++ search_es/indexes.py
@@ -1,6 +1,5 @@
 from .documents.gene import GeneDocument
 from .documents.metabolite import MetaboliteDocument
-from .documents.platform import PlatformDocument
 from .documents.protein import ProteinDocument
 from .documents.score import ScoreDocument
 # from .documents.phecode import PhecodeDocument
