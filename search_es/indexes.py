d9e3bc5b067cd14a2d151d5e4e0ac7a61d3368bd
ens-lgil <lg10@sanger.ac.uk>
2025-05-02 14:00:54 +0100

    Add Tissue to search indexes

--- search_es/indexes.py
+++ search_es/indexes.py
@@ -3,5 +3,6 @@ from .documents.metabolite import MetaboliteDocument
 from .documents.pathway import PathwayDocument
 from .documents.protein import ProteinDocument
 from .documents.score import ScoreDocument
+from .documents.tissue import TissueDocument
 
 # from .documents.phenotype import PhenotypeDocument
